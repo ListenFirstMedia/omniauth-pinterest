2f348f4d45da8027641308a4c137a2a2a56f87a4
Anthony Battaglia <anthony.battaglia@listenfirstmedia.com>
2023-09-25 17:31:05 +0000

    port api v5 changes

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -4,9 +4,10 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.pinterest.com/',
-        :authorize_url => 'https://api.pinterest.com/oauth/',
-        :token_url => 'https://api.pinterest.com/v1/oauth/token'
+        site: 'https://www.pinterest.com/',
+        authorize_url: 'https://www.pinterest.com/oauth/',
+        token_url: 'https://api.pinterest.com/v5/oauth/token',
+        auth_scheme: :basic_auth
       }
 
       def request_phase
@@ -18,22 +19,27 @@ module OmniAuth
       uid { raw_info['id'] }
 
       info { raw_info }
-      
+
       def authorize_params
         super.tap do |params|
-          %w[redirect_uri].each do |v| 
+          %w[redirect_uri].each do |v|
             params[:redirect_uri] = request.params[v] if request.params[v]
-          end 
-        end 
-      end 
+          end
+        end
+      end
 
       def raw_info
-        fields = 'first_name,id,last_name,url,account_type,username,bio,image'
-        @raw_info ||= access_token.get("/v1/me/?fields=#{fields}").parsed['data']
+        @raw_info ||= access_token.get('/v5/user_account/').parsed
       end
 
       def ssl?
         true
+      end
+
+      def build_access_token(*args)
+        t = super(*args)
+        t.client.site = 'https://api.pinterest.com/'
+        t
       end
 
     end
